b552337413a977eb6fb174ec05a1d1d887c3ccf1
caixh <153012533@qq.com>
2019-11-21 22:24:36 +0800

    vue-ssr学习

---
+++ server/index2.js
@@ -0,0 +1,44 @@
+//nodejs服务器
+const express = require('express')
+const Vue = require('vue');
+const fs = require('fs');
+
+//创建express实例和vue实例
+const app = express()
+//创建渲染器
+const {
+  createBundleRenderer
+} = require('vue-server-renderer');
+const serverBundle = require('../dist/server/vue-ssr-server-bundle.jsonn');
+const clientManifest = require('../dist/client/vue-ssr-server-manifest.json');
+const renderer = createBundleRenderer(serverBundle, {
+  runInNewContext: false,
+  template: fs.readFileSync('../public/index.temp.html', 'utf-8'),
+  clientManifest
+})
+
+//中间件处理静态文件请求
+app.use(express.static('../dist/client', {
+  index: false
+}))
+//路有处理交给vue
+
+app.get('*', async (req, res) => {
+  try {
+    const context = {
+      url: req.url,
+      title: 'ssr test'
+    }
+    const html = await renderer.renderToString(context)
+    //eslint-disable-next-line no-console
+    console.log(html)
+    res.send(html)
+  } catch (error) {
+    res.status(500).send('服务器内部错误')
+  }
+})
+
+app.listen(9090, () => {
+  //eslint-disable-next-line no-console
+  console.log('渲染器启动成功')
+})
